336b22a55dd12adb2a49c5a6ef8bf97a114da729
aserafin <adrian@softmad.pl>
2021-12-23 19:04:02 +0100

    set the default values for backwards compatibility

--- lib/action_encrypt/configuration.rb
+++ lib/action_encrypt/configuration.rb
@@ -3,5 +3,20 @@
 module ActionEncrypt
   class Configuration
     attr_accessor :key_encryption_key, :blind_index_key
+
+    def initialize(key_encryption_key: nil, blind_index_key: nil)
+      @key_encryption_key = key_encryption_key.presence || default_key_encryption_key
+      @blind_index_key = blind_index_key.presence || default_blind_index_key
+    end
+
+    private
+
+    def default_key_encryption_key
+      Rails.application.credentials.key_encryption_key
+    end
+
+    def default_blind_index_key
+      Rails.application.credentials.blind_index_key
+    end
   end
 end
