6fdd97d9406f23db28e8b5af58790ad78eec43cc
David Van Der Beek <earlynovrock@gmail.com>
2022-04-11 14:39:42 -0700

    fix for rails 7

--- app/models/action_encrypt/application_record.rb
+++ app/models/action_encrypt/application_record.rb
@@ -1,5 +1,21 @@
 module ActionEncrypt
   class ApplicationRecord < ActiveRecord::Base
     self.abstract_class = true
+
+    if defined?(ActiveRecord::Encryption) == 'constant'
+      @encrypted_attributes = {}
+
+      def self.encrypted_attributes
+        @encrypted_attributes ||= superclass.encrypted_attributes.dup
+      end
+
+      def encrypted_attributes
+        @encrypted_attributes ||= begin
+          duplicated= {}
+          self.class.encrypted_attributes.map { |key, value| duplicated[key] = value.dup }
+          duplicated
+        end
+      end
+    end
   end
 end
